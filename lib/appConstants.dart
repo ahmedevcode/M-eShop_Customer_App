8bc93551018e8826a28cae238056f0b4a0d332d2
ahmed <ahmedthurwat@gmail.com>
2024-10-10 06:26:11 +0300

    core_app

---
+++ lib/appConstants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String app_name = 'Qarib';
+  static const String androidPackageName = 'eShop.multivendor.customer';
+  static const String iosPackageName = 'wrteam.eshop.multivendor';
+  static const String baseUrl = 'https://vendor.eshopweb.store/app/v1/api/';
+  static const String chatBaseUrl =
+      'https://vendor.eshopweb.store/app/v1/Chat_Api/';
+  static const String shareNavigationWebUrl = 'vendor.eshopweb.store';
+  static const String defaultLanguage = 'en';
+}
